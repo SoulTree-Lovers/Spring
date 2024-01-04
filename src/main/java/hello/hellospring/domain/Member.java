7043633fa52a133fe2a422996b443e90dfb4e726
강승민 <rkdtmdals999@naver.com>
2024-01-04 23:45:37 +0900

    [feat] Jpa를 사용한 DB 접근

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,16 @@
 package hello.hellospring.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 알아서 id를 생성해줌.
     private Long id;
+
     private String name;
 
     public Long getId() {
